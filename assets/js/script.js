ad95e93d7a9c12e651e830498cde125f334833cb
dannyrgd <danny.ramirezgd@gmail.com>
2022-03-27 13:01:01 -0400

    added new structure and updated front end

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction () {
+//     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//         document.getElementById('navbar').
+//     }
+// }
